b2aea40182ce0a8a736dfaa662c3b2e32da91995
Laura Kehrl <kehrl@uw.edu>
2015-05-19 22:54:38 -0700

    added option for height above geoid or ellipsoid

--- Tools/helheim_bed.py
+++ Tools/helheim_bed.py
@@ -6,31 +6,32 @@ import os
 import sys
 import numpy as np
 sys.path.append(os.path.join(os.getenv("HOME"),"Code/Util/Modules"))
-sys.path.append(os.path.join(os.getenv("HOME"),"Code/Helheim/Observations/Elevation"))
+sys.path.append(os.path.join(os.getenv("HOME"),"Code/Helheim/Tools"))
 import coords, helheim_elevation
 import scipy.interpolate
 import geotiff
 
-def cresis(year):
+def cresis(year,verticaldatum):
 
   if year == '2001':
     file=os.path.join(os.getenv("HOME"),"Data/Bed/Cresis/helheim_flightline_05212001_good_wgs84.dat")
     data=np.loadtxt(file,skiprows=1)
-    y=data[3180:3297,0]
-    x=data[3180:3297,1]
-    H=data[3180:3297,3]
+    ind=range(3180,3297)
+    y=data[:,0]
+    x=data[:,1]
+    H=data[:,3]
     x2,y2 = coords.convert(x,y,4326,3413)
     
     surf=helheim_elevation.atm('2001')
     zs = scipy.interpolate.griddata(surf['20010521'][:,0:2],surf['20010521'][:,2],np.column_stack([x2,y2]),method='nearest')
-    bed = np.column_stack([x2,y2,zs-H,zs])
+    zb = zs-H
   
   else:
     print "Using data set Helheim_2008_2012_Composite"
     # New data set
-    #file=os.path.join(os.getenv("HOME"),"Data/Bed/Cresis/Helheim_2006_2013_Composite/flightlines/Helheim_2006_2013_Composite_Flightlines.txt")
+    file=os.path.join(os.getenv("HOME"),"Data/Bed/Cresis/Helheim_2006_2014_Composite/flightlines/Helheim_2006_2014_Composite_Flightlines.txt")
     # Old data set
-    file=os.path.join(os.getenv("HOME"),"Data/Bed/Cresis/Helheim_2008_2012_Composite/flightlines/Helheim_2008_2012_Composite_Flightlines.txt")
+    #file=os.path.join(os.getenv("HOME"),"Data/Bed/Cresis/Helheim_2008_2012_Composite/flightlines/Helheim_2008_2012_Composite_Flightlines.txt")
     fid = open(file,"r")
     x=[]
     y=[]
@@ -40,7 +41,10 @@ def cresis(year):
     lines = fid.readlines()
     for line in lines[1:-1]:
       fields = line.split()
-      years.append(float(fields[8][0:4]))
+      try:
+        years.append(float(fields[8][0:4]))
+      except:
+        years.append(0)
       x.append(float(fields[5]))
       y.append(float(fields[4]))
       zb.append(float(fields[1]))
@@ -51,7 +55,7 @@ def cresis(year):
     zb=np.array(zb)
     zs=np.array(zs)
     
-    x,y = coords.convert(x,y,4326,3413)
+    x2,y2 = coords.convert(x,y,4326,3413)
     
     if year == '2008':
       ind=range(32654,33126)
@@ -69,17 +73,44 @@ def cresis(year):
       ind=range(0,len(x))
     else:
       print "Unrecognized CreSIS profile"
+  
+  # Select what reference we want for the elevation  
+  if verticaldatum == "geoid":
+    print "working"
+    geoidheight = coords.geoidheight(x2,y2)
+    print geoidheight
+    zb = zb-geoidheight
+  elif verticaldatum == "ellipsoid":
+    zb = zb
+  else:
+    print "Unknown vertical datum, defaulting to ellipsoid height"
     
-    bed = np.column_stack([x[ind],y[ind],zb[ind]]) 
+  return np.column_stack([x2[ind],y2[ind],zb[ind]])
 
-  return bed
-
-def morlighem(xpts,ypts):
-  file = os.path.join(os.getenv("HOME"),"Data/Bed/Morlighem_2014/morlighem_bed.tif")
+def morlighem_pts(xpts,ypts,verticaldatum):
+  
+  # Load bed DEM
+  file = os.path.join(os.getenv("HOME"),"Data/Bed/Morlighem_2014/MCdataset-2015-04-27.tif")
   [x,y,z]=geotiff.read(file)
   
-  f = scipy.interpolate.RectBivariateSpline(y,x,z)
-  bed = f.ev(ypts,xpts)
+  f = scipy.interpolate.RegularGridInterpolator((y,x),z)
+  bed = f(np.column_stack([ypts,xpts]))
   bed[bed<-2000]='NaN'
+  
+  if verticaldatum == "geoid":
+    geoidheight = coords.geoidheight(xpts,ypts)
+    bed = bed-geoidheight
+  elif verticaldatum == "ellipsoid":
+    bed = bed
+  else:
+    print "Unknown vertical datum, defaulting to ellipsoid height"
       
   return bed
+  
+def morlighem_grid(xmin,xmax,ymin,ymax):
+
+  # Load Bed DEM
+  file = os.path.join(os.getenv("HOME"),"Data/Bed/Morlighem_2014/MCdataset-2015-04-27.tif")
+  [x,y,z]=geotiff.read(file,xmin,xmax,ymin,ymax)
+  
+  return x,y,z
