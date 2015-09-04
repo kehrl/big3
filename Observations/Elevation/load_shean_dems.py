3efd45803eae1798e070e1a6041070789c13d2d2
Laura Kehrl <kehrl@uw.edu>
2015-09-04 13:46:12 -0700

    still a work in progress

--- Observations/Elevation/load_shean_dems.py
+++ Observations/Elevation/load_shean_dems.py
@@ -5,11 +5,16 @@
 #
 import os, sys, shutil
 
+glacier = 'Helheim'
+
 # Directory where data is located
-TOPDIR = "/Volumes/insar3/dshean/helheim_kanger_se/dem/"
+if glacier == 'Kanger':
+  TOPDIR = "/Volumes/insar3/dshean/helheim_kanger_se/dem/kanger/front/"
+else: 
+  TOPDIR = "/Volumes/insar3/dshean/helheim_kanger_se/dem/helheim_front/"
 
 # Directory where we want to put the DEMs
-OUTDIR = os.path.join(os.getenv("HOME"),"Data/Elevation/Worldview/HelheimKanger/")
+OUTDIR = os.path.join(os.getenv("HOME"),"Data/Elevation/Worldview/"+glacier+"/")
 
 # Get contents of directory
 try:
@@ -19,10 +24,15 @@ except:
 
 # Copy 32 m DEMs to my local machine
 for DIR in DIRs:
-  if DIR.startswith("WV"):
-    dir = os.listdir(TOPDIR+DIR)
-    files = os.listdir(TOPDIR+DIR+"/"+dir[0])
-    for file in files:
-      if file.endswith('DEM_32m.tif'):
-        shutil.copy(TOPDIR+DIR+"/"+dir[0]+"/"+file,OUTDIR+file)
+  if glacier == 'Kanger':
+    if DIR.endswith("_align"):
+      dir = os.listdir(TOPDIR+DIR)
+      files = os.listdir(TOPDIR+DIR)
+      for file in files:
+        if file.endswith('trans_reference-DEM_32m.tif'):
+          print file
+          shutil.copy(TOPDIR+DIR+"/"+file,OUTDIR+file)
+  if glacier == 'Helheim':
+    if DIR.endswith("DEM_32m_trans.tif"):
+       shutil.copy(TOPDIR+DIR,OUTDIR+DIR)
   
