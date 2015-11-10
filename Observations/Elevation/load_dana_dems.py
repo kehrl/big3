4ce105d224a5a075511146cc3641edb934bcbd4b
kehrl <kehrl@uw.edu>
2015-11-10 09:22:17 -0800

    fixed some typos

--- Observations/Elevation/load_dana_dems.py
+++ Observations/Elevation/load_dana_dems.py
@@ -6,9 +6,9 @@
 import os
 
 glacier = "Helheim"
-TOPDIR = "/Volumes/insar4/ian/TSX/Helheim/DEM/fromDana/"
-OUTDIR =  os.path.join(os.getenv("HOME"),"bigtmp/shean_tdx")
-
+#TOPDIR = "/Volumes/insar4/ian/TSX/Helheim/DEM/fromDana/"
+TOPDIR = os.path.join("/Volumes/kehrl/Bigtmp/TDX/fromDana/")
+OUTDIR =  os.path.join("/Volumes/kehrl/Bigtmp/shean_tdx/")
 
 # Get list of directories
 dirs = os.listdir(TOPDIR)
@@ -22,8 +22,9 @@ for dir in dirs:
     for file in files:
       if file.endswith('_DEM.ras'):
         newfile = OUTDIR+file[25:33]+'_'+file[34:38]+'_'+dir[0:7]+'_'+file[41:-4]+'.tif'
-        os.system('gdalwarp -t_srs +proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs -et 0 -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=IF_SAFER -r cubic '+TOPDIR+dir+'/'+file+' '+newfile)
-        os.system('gdaladdo_lzw.py '+newfile)
+        if not(os.path.isfile(newfile)):
+          os.system("gdalwarp -t_srs '+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' -et 0.0 -r cubic -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=IF_SAFER "+TOPDIR+dir+'/'+file+' '+newfile)
+          os.system('gdaladdo.py '+newfile)
     if (dir[0:7] not in tracklist):
       tracklist.append(dir[0:7])
 
@@ -37,3 +38,5 @@ for track in tracklist:
   os.system('gdalwarp -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=IF_SAFER -r cubic'+filestomosaic+' '+OUTDIR+fileout)
 
 # Use Shean's demtools mask_raster.sh to burn mask out bad sections before aligning the DEM.
+
+#-t_srs '+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs' -et 0 -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=IF_SAFER -r cubic 
