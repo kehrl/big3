d4d0e75ae181dde75f7d04ee567267df2df5e41b
Laura Kehrl <kehrl@uw.edu>
2015-11-11 10:18:37 -0800

    added option to load_grid to also mask fjord according to ice front position

--- Tools/icemask.py
+++ Tools/icemask.py
@@ -11,22 +11,25 @@ LMK, UW, 9/15/2015
 import os
 import sys
 sys.path.append(os.path.join(os.getenv("CODE_HOME"),"Util/Modules"))
-import elmer_mesh
+import elmer_mesh, icefronts
 import matplotlib.path as path
 import numpy as np
 
-def load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0):
+def load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0,icefront_time='none',icefront_type='all'):
 
   '''
   Load ice mask grid.
   
-  x,y,mask = load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0)
+  x,y,mask = load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0,icefront_time='none')
   
   Inputs:
   glacier: glacier name
   xmin,xmax,ymin,ymax: output dimensions for grid
   dx: grid spacing
   ice: set ice grid points to 0 or 1
+  icefront_time: sometimes we will want the fjord to be part of the mask, so 
+  	this option also masks the fjord according to the ice front for the given time
+  
   
   Outputs:
   x,y: grid points
@@ -35,7 +38,6 @@ def load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0):
 
   # Directory for holes for mask
   DIR = os.path.join(os.getenv("DATA_HOME"),'Shapefiles/IceMasks/'+glacier+'/')
-
   files = os.listdir(DIR)
   
   # Set up grid
@@ -56,7 +58,7 @@ def load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0):
   maskflatten = mask.flatten()
   
   for file in files:
-    if file.endswith('.shp'):
+    if file.endswith('.shp') and 'hole' in file:
       hole = elmer_mesh.shp_to_xy(DIR+file)
       holepath = path.Path(hole[:,0:2])
       cond = holepath.contains_points(points)
@@ -65,6 +67,36 @@ def load_grid(glacier,xmin,xmax,ymin,ymax,dx,ice=0):
         maskflatten[ind]=0
       else: 
         maskflatten[ind]=1
+ 
+  # If we want to mask out icefree fjord
+  if icefront_time !='none':
+    xfjord = []
+    yfjord = []
+    xfront,yfront,time = icefronts.near_time(icefront_time,glacier,type=icefront_type)
+    south = elmer_mesh.shp_to_xy(DIR+'icemask_southfjordwall.shp')
+    north = elmer_mesh.shp_to_xy(DIR+'icemask_northfjordwall.shp')
+    if north[0,1] > north[-1,0]:
+      north = np.flipud(north)
+    if south[0,1] < south[-1,0]:
+      south = np.flipud(south) 
+    if yfront[0] > yfront[-1]:
+      xfront = np.flipud(xfront)
+      yfront = np.flipud(yfront)  
+    xfjord = xfront
+    yfjord = yfront
+    nind = np.argmin((xfront[-1]-north[:,0])**2+(yfront[-1]-north[:,1])**2)
+    xfjord = np.r_[xfjord,north[nind:,0]]
+    yfjord = np.r_[yfjord,north[nind:,1]]
+    sind = np.argmin((xfront[0]-south[:,0])**2+(yfront[0]-south[:,1])**2)
+    xfjord = np.r_[xfjord,south[0:sind+1,0]]
+    yfjord = np.r_[yfjord,south[0:sind+1,1]]
+    fjordpath = path.Path(np.column_stack([xfjord,yfjord]))
+    cond = fjordpath.contains_points(points)
+    ind = np.where(cond==True)[0] 
+    if ice == 1:
+      maskflatten[ind]=0
+    else: 
+      maskflatten[ind]=1
           
   mask = np.reshape(maskflatten,[ny,nx])    
 
