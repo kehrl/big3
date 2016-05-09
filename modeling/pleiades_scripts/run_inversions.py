0d21992b33153f18b7effac84392ce464c721475
Laura Kehrl <kehrl@uw.edu>
2016-05-09 14:19:44 -0700

    now using subprocess to submit command, hopefully it works!

--- modeling/pleiades_scripts/run_inversions.py
+++ modeling/pleiades_scripts/run_inversions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
  
-from popen2 import popen2
+import subprocess
 import time
+import os
 
 # Date for mesh
 date = '20120624'
@@ -38,8 +39,10 @@ else:
 # Create mesh #
 ###############
 
-command = "python /u/lkehrl/Code/big3/modeling/meshing/"+\
-  "helheim_mesh_3d.py -mesh {0} -d {1} -bname {2} -bmodel {3} -bsmooth {4} -lc {5} -n {6} -output {7}".format(meshshp,date,bname,bmodel,bsmooth,lc,nparts,meshname)
+#command = "python /u/lkehrl/Code/big3/modeling/meshing/"+\
+command = "python helheim_mesh_3d.py -mesh {0} -d {1} -bname {2} -bmodel {3} -bsmooth {4} -lc {5} -n {6} -output {7}".format(meshshp,date,bname,bmodel,bsmooth,lc,nparts,meshname)
+
+os.system(command)
 
 ############################
 # Submit inversions to PBS #
@@ -49,15 +52,15 @@ command = "python /u/lkehrl/Code/big3/modeling/meshing/"+\
 for regpar in regpars:
  
     # Open a pipe to the qsub command.
-    output, input = popen2('qsub')
+    #output, input = popen2('qsub')
      
     # Customize your options here
     job_name = "lambda_%s" % regpar
-    walltime = "6:00:00"
+    walltime = "1:00:00"
     processors = "select={0}:ncpus={1}:mpiprocs={2}:model={3}".format(nparts/ncpus,ncpus,ncpus,model)
     command = "python /u/lkehrl/Code/big3/modeling/inversions/helheim_inversion_3d.py"+\
               " -method {0} -regpar {1} -mesh {2} -extrude {3} -front {4}".format(method,regpar,meshname,extrude,frontBC)
-    dir = "/nobackupp8/lkehrl/Models/"+glacier+"/3D/"+meshname
+    dir = "/nobackupp8/lkehrl/Models/"+glacier+"/3D/"+meshname+"/"
  
     job_string = """
     #PBS -S /bin/bash
@@ -66,17 +69,18 @@ for regpar in regpars:
     #PBS -N %s
     #PBS -l walltime=%s
     #PBS -l %s
-    #PBS -o ./output/%s.out
-    #PBS -e ./error/%s.err
+    #PBS -o %s
+    #PBS -e %s
     cd %s
-    %s""" % (job_name, walltime, processors, job_name, job_name,dir,command)
+    %s""" % (job_name, walltime, processors, dir+job_name+".out",dir+job_name+".err",dir,command)
      
-    # Send job_string to qsub
-    input.write(job_string)
-    input.close()
-     
-    # Print your job and the system response to the screen as it's submitted
-    print job_string
-    print output.read()
+    os.chdir(dir)
+    fid = open("PBS_"+method+"_"+regpar+".pbs")
+    fid.write(job_string)
+    fid.close()
+    try:
+      subprocess.call('qsub < "PBS_"+method+"_"+regpar+".pbs"')
+    finally:
+      os.remove("PBS_"+method+"_"+regpar+".pbs")
      
     time.sleep(0.1)
