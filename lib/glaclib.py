07b5bfd722c34fd02b81c15a72dd4b11fa5c3baa
Laura Kehrl <kehrl@uw.edu>
2017-05-15 15:12:01 -0700

    no reason to use inversion_3d in calculation of ice front position through time, now using the annual means from 2008-2016

--- lib/glaclib.py
+++ lib/glaclib.py
@@ -226,9 +226,11 @@ def load_extent_timeseries(glacier,time1,time2,dt,nofront_shapefile='glacier_ext
 
   # Load a velocity profile to use as interpolation direction to figure out ice-front position
   # on timesteps that fall between picked ice fronts
-  x = np.arange(np.nanmin(termx)-1.0e3,np.nanmax(termx)+1.0e3,100.)
-  y = np.arange(np.nanmin(termy)-1.0e3,np.nanmax(termy)+1.0e3,100.)
-  u,v = vellib.inversion_3D(glacier,x,y,(time1+time2)/2)
+  #x = np.arange(np.nanmin(termx)-1.0e3,np.nanmax(termx)+1.0e3,100.)
+  #y = np.arange(np.nanmin(termy)-1.0e3,np.nanmax(termy)+1.0e3,100.)
+  x,y,u = geotifflib.read(os.path.join(os.getenv("DATA_HOME"),"Velocity/TSX/"+glacier+"/TIF/all-2008-2016_vx.tif"))
+  x,y,v = geotifflib.read(os.path.join(os.getenv("DATA_HOME"),"Velocity/TSX/"+glacier+"/TIF/all-2008-2016_vy.tif"))  
+  #u,v = vellib.inversion_3D(glacier,x,y,(time1+time2)/2)
   fu = scipy.interpolate.RegularGridInterpolator((y,x),u)
   fv = scipy.interpolate.RegularGridInterpolator((y,x),v)
 
