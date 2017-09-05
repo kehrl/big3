d9b28eb49721f5ef9137e83941c117973e2eaa5b
kehrl <kehrl@uw.edu>
2017-09-05 09:34:41 -0700

    made it possible to restart simulation for runs other than terminusdriven

--- modeling/simulations/simulation_3d.py
+++ modeling/simulations/simulation_3d.py
@@ -24,7 +24,7 @@ parser.add_argument("-n", dest="n", required = True,
         help = "Number of partitions.")
 parser.add_argument("-extrude", dest="extrude", type=int,required = False,\
        default=10,help = "Number of extrusion levels.")
-parser.add_argument("-restartsolverfile",dest="restartfile",required = False,\
+parser.add_argument("-restartfile",dest="restartfile",required = False,\
        default="none",help = "Name of restart solver file (if applicable).")
 parser.add_argument("-restartposition",dest="restartposition",required = False,\
        default=0,type=int,help = "Restart position in results file (if applicable).")
@@ -62,10 +62,16 @@ DIRR=os.path.join(DIRM+"mesh2d/simulation/")
 inputs=os.path.join(DIRM+"inputs/")
 
 # Check that solver file exists and remove the '.sif' at the end, if it's present
-if solverfile_in.endswith('.sif'):
-  solverfile_in = solverfile_in[0:-4]
-if not(os.path.exists(DIRS+solverfile_in+'.sif')):
-  sys.exit('No solverfile with name '+solverfile_in+'.sif')
+if restartfile !='none':
+  solverfile_in = restartfile
+  date = restartfile[-12:-4]
+  if solverfile_in.endswith('.sif'):
+    solverfile_in = solverfile_in[0:-4]
+else:
+  if solverfile_in.endswith('.sif'):
+    solverfile_in = solverfile_in[0:-4]
+  if not(os.path.exists(DIRS+solverfile_in+'.sif')):
+    sys.exit('No solverfile with name '+solverfile_in+'.sif')
 
 # Grab boundary condition for front -- it will be different if we are using an actual
 # terminus position (neumann, pressure BC) or an outflow boundary (dirichlet, velocity BC)
@@ -115,30 +121,33 @@ if restartposition > 0:
 # Change mesh file in solver file #
 ###################################
 
-# Get current date
-now = datetime.datetime.now()
-date = '{0}{1:02.0f}{2:02.0f}'.format((now.year),(now.month),(now.day))
+if restartfile !='none':
+  solverfile_out = solverfile_in
+else:
+  # Get current date
+  now = datetime.datetime.now()
+  date = '{0}{1:02.0f}{2:02.0f}'.format((now.year),(now.month),(now.day))
  
-os.chdir(DIRM)
-fid1 = open(DIRS+solverfile_in+'.sif', 'r')
-solverfile_out = solverfile_in+'_'+date
-fid2 = open(DIRM+solverfile_out+'.sif', 'w')
+  os.chdir(DIRM)
+  fid1 = open(DIRS+solverfile_in+'.sif', 'r')
+  solverfile_out = solverfile_in+'_'+date
+  fid2 = open(DIRM+solverfile_out+'.sif', 'w')
 
-lines=fid1.read()
-lines=lines.replace('{Extrude}', '{0}'.format(extrude))
-lines=lines.replace('{FrontBC}', '{0}'.format(frontbc_text))
-lines=lines.replace('{Temperature}', '{0}'.format(temperature_text))
-lines=lines.replace('{TimeStepSize}', '$({0})'.format(dt))
-lines=lines.replace('{TimeSteps}', '{0}'.format(nt))
-lines=lines.replace('{SlipCoefficient}', '{0}'.format(slipcoefficient))
-lines=lines.replace('{BetaDataFile}', '{0}'.format(beta_data_file))
-lines=lines.replace('{SlidingExponent}', '{0}'.format(sliding_exponent))
-lines=lines.replace('{nrestart}','{0}'.format(restartposition))
+  lines=fid1.read()
+  lines=lines.replace('{Extrude}', '{0}'.format(extrude))
+  lines=lines.replace('{FrontBC}', '{0}'.format(frontbc_text))
+  lines=lines.replace('{Temperature}', '{0}'.format(temperature_text))
+  lines=lines.replace('{TimeStepSize}', '$({0})'.format(dt))
+  lines=lines.replace('{TimeSteps}', '{0}'.format(nt))
+  lines=lines.replace('{SlipCoefficient}', '{0}'.format(slipcoefficient))
+  lines=lines.replace('{BetaDataFile}', '{0}'.format(beta_data_file))
+  lines=lines.replace('{SlidingExponent}', '{0}'.format(sliding_exponent))
+  lines=lines.replace('{nrestart}','{0}'.format(restartposition))
 
-fid2.write(lines)
-fid1.close()
-fid2.close()
-del fid1, fid2
+  fid2.write(lines)
+  fid1.close()
+  fid2.close()
+  del fid1, fid2
 
 #####################################
 # Start zip/unzip process if wanted #
