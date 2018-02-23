6389646a9ba18df5f2ec8479f563f94ee8230bfe
kehrl <kehrl@uw.edu>
2018-02-23 10:10:44 -0800

    new option to use weertman sliding law with SSA simulations

--- modeling/simulations/simulation_ssa.py
+++ modeling/simulations/simulation_ssa.py
@@ -36,6 +36,8 @@ parser.add_argument("-nt",dest="nt",required  = False,type=str,\
        default='10',help = "Number of timesteps (10).") 
 parser.add_argument("-dt",dest="dt",required  = False,type=str,\
        default='1/365.25',help = "Timestep size (1/365.25, i.e., 1 day).") 
+parser.add_argument("-slidinglaw",dest="slidinglaw",required  = False,type=str,\
+       default='linear',help = "Sliding law (linear or weertman).")
 
 args, _ = parser.parse_known_args(sys.argv)
 
@@ -51,6 +53,7 @@ temperature = args.temperature
 dt = args.dt
 nt = args.nt
 beta_suffix = args.beta_suffix
+slidinglaw = args.slidinglaw
 
 # Directories
 DIRS=os.path.join(os.getenv("CODE_HOME"),"big3/modeling/solverfiles/SSA/")
@@ -76,21 +79,21 @@ if not(beta_suffix==""):
 # Grab boundary condition for front -- it will be different if we are using an actual
 # terminus position (neumann, pressure BC) or an outflow boundary (dirichlet, velocity BC)
 if (frontbc == 'neumann') or (frontbc == 'pressure'):
-  frontbc_text ="""
+    frontbc_text ="""
   Flow Force BC = Logical True
   External Pressure = Variable Coordinate 3 !we are in MPa units
   Real MATC "-1.0*waterpressure(tx)*1.0E-06" """
 elif (frontbc == 'dirichlet') or (frontbc == 'velocity'):
-  frontbc_text = """
+    frontbc_text = """
   Velocity 1 = Variable Coordinate 1
     Real procedure "USF_Init.so" "UWa"
   Velocity 2 = Variable Coordinate 1
     Real procedure "USF_Init.so" "VWa"""
 else:
-  sys.exit("Unknown BC for front of glacier.")
+    sys.exit("Unknown BC for front of glacier.")
 
 if temperature == 'model':
-  temperature_text="""
+    temperature_text="""
   mu = Variable Coordinate 1, Coordinate 2\r
     Real Procedure "USF_Init.so" "SSAViscosity"""""
 else:
@@ -101,6 +104,23 @@ else:
     mu = Real $(("""+'2*'+str(E)+'*'+str(A[0])+'*'+'yearinsec)^(-1.0/3.0)*1.0e-6)'
   except:
     sys.exit("Unknown temperature of "+temperature)
+
+if slidinglaw == 'linear':
+    sliding_text = """
+  SSA Friction Law = String "linear"
+  SSA Friction Parameter = Variable Coordinate 1, Coordinate 2
+    REAL Procedure "Sliding_Beta.so" "Linear_NearestNeighbor" """
+elif slidinglaw == 'weertman':
+    sliding_text = """
+  SSA Friction Law = String "weertman"  
+  SSA Friction Parameter = Variable Coordinate 1, Coordinate 2
+    REAL Procedure "Sliding_Beta.so" "Weertman_NearestNeighbor"
+  ! Exponent m
+  SSA Friction Exponent = Real $(1.0/3.0)
+  ! Min velocity for linearisation where ub=0
+  SSA Friction Linear Velocity = Real 0.0001 """
+else:
+    sys.exit("Unknown sliding law of "+slidinglaw)
 
 ###################################
 # Change mesh file in solver file #
@@ -123,6 +143,7 @@ lines=lines.replace('{Temperature}', '{0}'.format(temperature_text))
 lines=lines.replace('{TimeStepSize}', '$({0})'.format(dt))
 lines=lines.replace('{TimeSteps}', '{0}'.format(nt))
 lines=lines.replace('{steady_name}','steady{0}'.format(beta_suffix))
+lines=lines.replace('{SlidingLaw}', '{0}'.format(sliding_text))
 
 fid2.write(lines)
 fid1.close()
