f032c8c15e402bbf02587b974b33ee229cdf637f
kehrl <kehrl@uw.edu>
2018-05-02 19:44:26 -0700

    added 20141016 to dates

--- modeling/pleiades_scripts/run_inversions.py
+++ modeling/pleiades_scripts/run_inversions.py
@@ -8,7 +8,6 @@ import os
 glacier = 'Helheim'
 
 # Mesh geometry
-
 meshshp = 'glacier_extent_inversion_front'
 extrude = 12
 #bname = 'smith'
@@ -16,13 +15,11 @@ bname = 'morlighem'
 bmodel = 'aniso'
 bsmooth = '5'
 bottomsurface = 'bed' # or 'iceshelf'
-temperature = '-10.0'#'-10.0'#'model'
-#lc = '300 300 300 300'
+temperature = 'model'#'-10.0'#'model'
 lc = '250 250 250 250'
-#lc = '1000 1000 4000 5000'
 
 if glacier == 'Helheim':
-  dates = ['20120316']
+  dates = ['20141016']
   #dates = ['20040803','20050829','20060825','20070912',\
   #         '20080814','20100624',\
   #         '20110319','20110615','20110828','20111116',\
@@ -30,22 +27,21 @@ if glacier == 'Helheim':
   #         '20130209','20130508','20130804','20131031',\
   #         '20140127','20140509','20140731','20141016']
 elif glacier == 'Kanger':
-  #dates = ['20010712','20030801','20050831','20060505',\
-  #	   '20060808','20060919','20090817','20100603']
-  dates = ['20120213']
-  #dates = ['20010712','20030801','20050621','20050831',\
-  #         '20060505','20060708','20060919,\
-  #         '20070728','20090817','20100603',\
-  #         '20110308','20110708','20110826','20111106',\
-  #         '20120213','20120522','20121012','20121217',\
-  #         '20130210','20130714','20131004','20131204',\
-  #         '20140213','20150808']  
+  #dates = ['20120522']
+  dates = ['20010712','20030801','20050621','20050831',\
+           '20060505','20060708','20060919',\
+           '20070728','20090817','20100603',\
+           '20110308','20110708','20110826','20111106',\
+           '20120213','20120522','20121012','20121217',\
+           '20130210','20130714','20131004','20131204',\
+           '20140213','20150808']  
 
 # Inversion options
 method = 'adjoint'
 itmax = 500
-regpars = ['1e11']
-#regpars = ['1e8','1e9','1e10','5e11','1e11','2e11','5e11','1e12','1e13','1e14'] 
+regpars = ['1e12']
+#regpars = ['1e9','5e9','1e10','5e10','1e11','5e11','1e12',\
+#	   '5e12','1e13','5e13','1e14','5e14'] 
 
 
 # Options for PBS submission
@@ -53,7 +49,7 @@ queue = 'normal'
 model = 'has'
 nparts = 24
 ncpus = 24
-runtime = '16:00:00'
+runtime = '36:00:00'
 
 if meshshp.endswith('nofront'):
   frontBC = 'pressure'
@@ -68,7 +64,7 @@ else:
 for date in dates:
 
   # Output mesh name
-  meshname = 'DEM'+date+'_constantT_steady'
+  meshname = 'DEM'+date+'_modelT'
 
   # Create mesh
   command = "python /u/lkehrl/Code/big3/modeling/meshing/"+\
@@ -76,7 +72,7 @@ for date in dates:
             " -d {0} -bname {1} -bmodel {2}".format(date,bname,bmodel,bsmooth)+\
             " -bsmooth {0} -lc {1} -n {2}".format(bsmooth,lc,nparts)+\
             " -output {0} -zb {1}".format(meshname,bottomsurface)+\
-            "  -temperature {0}".format(temperature)
+            " -temperature {0}".format(temperature)
   print command
   os.system(command)
 
